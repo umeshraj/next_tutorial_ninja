31202e64138a4baf7341ef4ec30d5a190d50aa66
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-20 05:26:00 -0500

    Creating about page

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
+        quibusdam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo doloremque
+        ipsa est consequuntur blanditiis sunt.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,14 @@ export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nihil
+        quasi illo earum magni nobis?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum
+        perferendis suscipit quasi reprehenderit nulla assumenda.
+      </p>
     </div>
   );
 }
